4f367ae0766a23d43a290c0b2b7bf37bdf656ba0
Rodrigo <rodrigo.travasso@gmail.com>
2022-08-04 18:49:42 -0300

    atualização de branch para operadores

---
+++ aula 10 - variáveis/index.js
@@ -0,0 +1,11 @@
+const primeiroNome = "Rodrigo";
+const sobrenome = "Travasso";
+const idade = 29;
+
+console.log(primeiroNome, sobrenome, idade)
+
+let numeros = [2,4,6,8,10]
+
+console.log(numeros)
+console.log(numeros[3])
+console.log("os prmeiros, terceiro e quinto elemetos do array são, ", numeros[0],numeros[2],numeros[4])
